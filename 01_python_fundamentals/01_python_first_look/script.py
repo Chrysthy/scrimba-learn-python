5d89a76df9b46a3ddc00ec4e14dee34179609fd0
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-01 17:52:24 -0300

    feat: add Python: First Look

---
+++ 01_python_fundamentals/01_python_first_look/script.py
@@ -0,0 +1,22 @@
+# Challenge: Try out the code!
+
+# 1. Change the program's variables so that the invitation prints your name and today's date.
+# 2. Change the dress code to "casual." No need to dress up to learn Python!
+# 3. Run the Python file (python .\script.py) to check your work!
+
+host = "Scrimba"
+date = "August 01"
+dress_code = "Casual"
+name = "Chrystine"
+
+print("=" * 40)
+print()
+print(f"  {name}, you are cordially invited to:")
+print()
+print("        LEARN PYTHON")
+print()
+print(f"  Hosted by: {host}")
+print(f"  Date: {date}")
+print(f"  Dress code: {dress_code}")
+print()
+print("=" * 40)
