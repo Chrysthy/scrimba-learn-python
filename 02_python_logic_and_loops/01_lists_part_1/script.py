a87460d37d5196d6cdf898625176e621682e2253
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-02 21:00:05 -0300

    feat: add List examples

---
+++ 02_python_logic_and_loops/01_lists_part_1/script.py
@@ -0,0 +1,27 @@
+# word1 = "apple"
+# word2 = "orange"
+# word3 = "banana"
+
+# List Examples
+fruits = ["apple", "orange", "banana"]
+ratings = [8, 9, 10]
+prices = [3.99, 4.99, 5.99]
+scores = [[2,3], [4,5,6], [77,88,99]]
+mood = ["☀️", "❤️", "🌈"]
+
+print(fruits)
+print(ratings)
+print(prices)
+print(scores)
+print(mood)
+
+
+# Challenge: Portfolio Data
+# You need some data to populate your personal portfolio site.
+
+# 1. Create a list called `skills` with 3 skills you have (or want to learn).
+# 2. Create a list called `years` with how many years you've spent on each skill.
+# 3. Create a list called `about_me` with at least 3 emojis that say something about your personality or hobbies. You can open the emoji picker to browse and insert emojis using these keyboard shortcuts:
+#   Mac: Control+Command+Space
+#   Windows: Windows+Period
+# 4. Print all three lists.
