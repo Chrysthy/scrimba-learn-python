6c31b2d1fc801a53bb5a1250596858c28e923319
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-07 20:40:41 -0300

    feat: add string methods examples

---
+++ 02_python_logic_and_loops/11_string_methods/script.py
@@ -0,0 +1,20 @@
+recipe = "chicken tikka masala"
+header = recipe.upper()
+
+print(recipe)
+print(header)
+
+search = "PASTA"
+print(search.lower())
+
+chef = "grace smith"
+print(chef.title())
+
+print("50%".strip('%'))
+
+recipe = "     tiramisu    "
+print(recipe.strip())
+
+raw = "    CHiCKen TIKKA MasALA"
+query = raw.strip().lower()
+print(query)
