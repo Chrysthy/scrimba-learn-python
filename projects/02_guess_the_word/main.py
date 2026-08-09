f9e9e0eedaf11e7da55722533dda298bef61b681
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-09 20:43:32 -0300

    feat: implement guessing logic in Guess the Word game

--- projects/02_guess_the_word/main.py
+++ projects/02_guess_the_word/main.py
@@ -1,3 +1,5 @@
+import random
+
 print("Welcome to Guess the Word!")
 print()
 print("Unscramble the letters and discover the hidden tech word.")
@@ -5,10 +7,19 @@ print("Unscramble the letters and discover the hidden tech word.")
 words = ["burger", "video game", "cat"]
 
 word = random.choice(words)
-print(word)
 
 letters = list(word)
 random.shuffle(letters)
 scrambled_word = "".join(letters)
 
 print(f"Scrambled: {scrambled_word}")
+
+guess = input(
+    f"type a guess or type 'skip' to skip the word: ").lower().strip()
+
+if guess == "skip":
+    print(f"Skipped! The word was {word}.")
+elif guess == word:
+    print("✅ Correct!")
+else:
+    print(f"❌ Sorry, the word was {word}.")
