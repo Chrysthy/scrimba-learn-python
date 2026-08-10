9f63d478f6c1acbf1a5fcb89cb83428cb45ee4cc
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-08-10 14:22:57 -0300

    feat: add while loops examples

---
+++ 02_python_logic_and_loops/15_while_loops/script.py
@@ -0,0 +1,25 @@
+count = 1
+
+while count <= 5:
+    print(count)
+    count += 1
+    # count = count + 1
+
+
+# forçar a parar
+while count <= 5:
+
+    if count == 3:
+        break
+
+    print(count)
+    count += 1
+  
+
+
+TOTAL_LAPS = 10 # contante (const)
+lap = 1
+
+while lap <= TOTAL_LAPS:
+    print(f"Lap: {lap}")
+    lap += 1
