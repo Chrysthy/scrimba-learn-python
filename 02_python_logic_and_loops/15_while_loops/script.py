3a96c66654bf64ecb7605e0810b396c3923f8cfe
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-08-10 14:28:29 -0300

    feat: add challenge solved

--- 02_python_logic_and_loops/15_while_loops/script.py
+++ 02_python_logic_and_loops/15_while_loops/script.py
@@ -36,3 +36,7 @@ while lap <= TOTAL_LAPS:
 
 PASSENGERS = 5
 passenger_num = 1
+
+while passenger_num <= PASSENGERS:
+    print(f"Printing boarding pass {passenger_num} of {PASSENGERS}...")
+    passenger_num += 1
