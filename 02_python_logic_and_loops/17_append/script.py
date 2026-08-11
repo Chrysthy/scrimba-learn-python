9c63853f2b76857499b7b3b7bcf12a4f979b0dbc
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-08-11 15:04:48 -0300

    Feat: add append examples

---
+++ 02_python_logic_and_loops/17_append/script.py
@@ -0,0 +1,14 @@
+tasks = []
+
+tasks.append("Send invoces")
+tasks.append("Book the venue")
+tasks.append("Reply to e-mails")
+
+
+task_item = input("Add an item: ")
+
+tasks.append(task_item)
+
+print(tasks)
+
+
