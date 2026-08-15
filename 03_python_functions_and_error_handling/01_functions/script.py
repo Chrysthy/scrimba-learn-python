85e6fcddec352771fbb83ae7c5ae1ada295f927e
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-15 13:51:19 -0300

    feat: add challenge

--- 03_python_functions_and_error_handling/01_functions/script.py
+++ 03_python_functions_and_error_handling/01_functions/script.py
@@ -30,3 +30,17 @@ def play_game():
    throw_dice()
 
 play_game()
+
+# Challenge: Write some functions!
+# You've very generously decided to pick up coffee for six of your favorite coworkers this morning. 
+
+# 1. Write a function announcing your coffee run. 
+# Example output: "I am headed to the coffee shop! Who wants a latte?"
+
+# 2. Write a function to calculate the total cost if you buy them each a latte for $5. In an f string, print how many lattes and what the total comes to.  
+# Example output: 
+# 6 lattes comes to $30.
+
+# 3. Your coworkers thank you profusely. To save a little time, write a function that prints "You're welcome!" twice, then call it as many times as you need to thank all six coworkers. 
+
+# 4. Put all your function calls in a new function called coffee_run(), and call it to start your coffee run!
