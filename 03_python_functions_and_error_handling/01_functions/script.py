4dbc344b5a20dc7a3c1bd6348b040e956144d8ef
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-15 14:10:10 -0300

    feat: add challenge solved

--- 03_python_functions_and_error_handling/01_functions/script.py
+++ 03_python_functions_and_error_handling/01_functions/script.py
@@ -4,43 +4,79 @@ import random
 # print(f"You rolled a {roll}")
 
 # def = significa definir uma função - define
+
+
 def roll_dice():
-    roll = random.randint(1,6)
+    roll = random.randint(1, 6)
     print(f"You rolled a {roll}")
+
 
 roll_dice()
 roll_dice()
 roll_dice()
 
 # função que chama outra função
+
+
 def throw_dice():
-  roll_dice()
-  roll_dice()
+    roll_dice()
+    roll_dice()
+
 
 throw_dice()
 
+
 def show_welcome():
-   print()
-   print("Welcome to Dice Fight!")
-   print()
+    print()
+    print("Welcome to Dice Fight!")
+    print()
 
 # A função play_game chama as funções show_welcome e throw_dice e executa o jogo
+
+
 def play_game():
-   show_welcome()
-   throw_dice()
+    show_welcome()
+    throw_dice()
+
 
 play_game()
 
 # Challenge: Write some functions!
-# You've very generously decided to pick up coffee for six of your favorite coworkers this morning. 
+# You've very generously decided to pick up coffee for six of your favorite coworkers this morning.
 
-# 1. Write a function announcing your coffee run. 
+# 1. Write a function announcing your coffee run.
 # Example output: "I am headed to the coffee shop! Who wants a latte?"
 
-# 2. Write a function to calculate the total cost if you buy them each a latte for $5. In an f string, print how many lattes and what the total comes to.  
-# Example output: 
+# 2. Write a function to calculate the total cost if you buy them each a latte for $5. In an f string, print how many lattes and what the total comes to.
+# Example output:
 # 6 lattes comes to $30.
 
-# 3. Your coworkers thank you profusely. To save a little time, write a function that prints "You're welcome!" twice, then call it as many times as you need to thank all six coworkers. 
+# 3. Your coworkers thank you profusely. To save a little time, write a function that prints "You're welcome!" twice, then call it as many times as you need to thank all six coworkers.
 
 # 4. Put all your function calls in a new function called coffee_run(), and call it to start your coffee run!
+
+
+def announce_coffee_run():
+    print("I am headed to the coffee shop! Who wants a latte?")
+
+
+def calc_coffee_total():
+    lattes = 6
+    cost_per_cup = 5
+    print(f"{lattes} lattes comes to ${lattes * cost_per_cup}.")
+
+
+def say_welcome():
+    print("You're welcome!")
+    print("You're welcome!")
+
+
+def coffee_run():
+    announce_coffee_run()
+    calc_coffee_total()
+    say_welcome()
+    say_welcome()
+    say_welcome()
+
+
+coffee_run()
