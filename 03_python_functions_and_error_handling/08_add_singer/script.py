42e27247999a797b474a36e0d06021086480ddaa
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-08-21 14:31:57 -0300

    feat: add a singer and prompt for singer

---
+++ 03_python_functions_and_error_handling/08_add_singer/script.py
@@ -0,0 +1,66 @@
+queue = [("Annie", "Dancing Queen"), ("Allen", "Country Roads")]
+
+def show_queue(queue):
+  """Print everyone currently in the queue"""
+  print()
+  print("Current queue:")
+  print()
+  for singer in queue:
+    name, song = singer
+    print(f"{name} - {song}")
+  print()
+  print("Options:  add / remove / quit")
+
+# prompt for singer 
+
+def prompt_for_singer():
+  """Ask for a name and a song, and return them cleaned up."""
+  name = input("Name: ").strip().title()
+  song = input("Song: ").strip().title()
+
+  return name, song
+
+def add_singer(queue):
+  """Ask for the singer and add them to the queue"""
+  name, song = prompt_for_singer()
+  print(name, song)
+
+  queue.append((name, song))
+  print()
+  print(f"Aded {name} to the queue")
+
+
+
+# Function stubs go here
+def add_singer(queue):
+  """Ask for the singer and add them to the queue"""
+  print("[add a singer]")
+
+
+def remove_singer(queue): 
+  """Remove singer fron the queue"""
+  print("[remove a singer]")
+
+def run_app(queue):
+    print("=" * 44)
+    print("Welcome to Sing Out: A Karaoke Queue Manager")
+    print("=" * 44)
+
+    is_running = True 
+
+    while is_running: 
+      show_queue(queue)
+      command = input("> ")
+
+      if command == "quit": 
+        is_running = False
+        print("The queue is closed. Good night!")
+      # your commands go here 
+      elif command == "add": 
+        add_singer(queue)
+      elif command == "remove":
+        remove_singer(queue)
+      else: 
+        print(f"Sorry, I don't know the command '{command}'")
+
+run_app(queue)
