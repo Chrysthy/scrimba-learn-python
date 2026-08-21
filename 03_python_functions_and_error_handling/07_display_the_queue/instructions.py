e62765e350c5d42594afffa14477e567fcf60a19
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-08-21 13:49:45 -0300

    feat: add instructions

---
+++ 03_python_functions_and_error_handling/07_display_the_queue/instructions.py
@@ -0,0 +1,20 @@
+Challenge: Display the Queue
+Replace show_queue()'s placeholder code with working code: 
+1. Add a header that says "Current Queue:". Give it some visual space by adding a blank
+   line above and below.
+2. Loop over the queue and print each singer's name and song. 
+3. Move the options menu into show_queue(), since we want to keep display items together and show the options menu each time we show the queue. 
+
+Example output:
+
+============================================
+Welcome to Sing Out: A Karaoke Queue Manager
+============================================
+
+Current Queue:
+
+Annie - Dancing Queen
+Allen - Country Roads
+
+Options: add / remove / quit
+>
