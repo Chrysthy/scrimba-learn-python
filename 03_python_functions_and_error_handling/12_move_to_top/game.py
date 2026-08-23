47f04558372bc782ed0dc7d4c218c889bfed6deb
Chrystine Martins <chrystine.martins@outlook.com>
2026-08-23 11:47:51 -0300

    feat: add move to top function

---
+++ 03_python_functions_and_error_handling/12_move_to_top/game.py
@@ -0,0 +1,87 @@
+queue = [("Annie", "Dancing Queen"), ("Allen", "Country Roads")]
+
+
+def show_queue(queue):
+    """Print everyone currently in the queue"""
+    print()
+    print("Current queue:")
+    print()
+    for i, singer in enumerate(queue):
+        name, song = singer
+        print(f"{i + 1}. {name} - {song}")
+    print()
+    print("Options:  add / next / top / remove / quit")
+
+
+def prompt_for_singer():
+    """Ask for a name and a song, and return them cleaned up."""
+    name = input("Name: ").strip().title()
+    song = input("Song: ").strip().title()
+    return name, song
+
+
+def add_singer(queue):
+    """Ask for the singer and add them to the queue"""
+    name, song = prompt_for_singer()
+    queue.append((name, song))
+    print()
+    print(f"Added {name} to the queue.")
+
+
+def next_singer(queue):
+    name, song = queue.pop(0)
+    print()
+    print(f"NOW UP: {name} - {song}")
+
+# add move_to_top
+def move_to_top(queue):
+    position = int(
+        input("Who do you want to move to the top? Enter a number: "))
+    print()
+
+    singer = queue.pop(position - 1)
+    queue.insert(0, singer)
+
+    name, song = singer
+    print(f"Moved {name} to the top of the queue!")
+
+
+def remove_singer(queue):
+    """Remove singer fron the queue"""
+    name = input("Who do you want to remove? ").strip().title()
+    print()
+    for singer in queue:
+        if singer[0] == name:
+            queue.remove(singer)
+            print(f"Removed {name} from the queue.")
+            return
+    print(f"There's no one named {name} in the queue.")
+
+
+def run_app(queue):
+    print("=" * 44)
+    print("Welcome to Sing Out: A Karaoke Queue Manager")
+    print("=" * 44)
+
+    is_running = True
+
+    while is_running:
+        show_queue(queue)
+        command = input("> ")
+
+        if command == "quit":
+            is_running = False
+            print("The queue is closed. Good night!")
+        elif command == "add":
+            add_singer(queue)
+        elif command == "next":
+            next_singer(queue)
+        elif command == "top":
+            move_to_top(queue)
+        elif command == "remove":
+            remove_singer(queue)
+        else:
+            print(f"Sorry, I don't know the command '{command}'")
+
+
+run_app(queue)
